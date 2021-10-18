ddf6dd7e3bbaf13a3724060bb647a6ecc8da6c93
jrazap <elazapm@gmail.com>
2021-10-18 07:54:54 +0200

    Vue Component Added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+Vue.component("fixed-part", {
+  props: {
+    layer: {},
+  },
+  template: `<div class="side-content">
+  <div class="line1"></div>
+  <ul class="social">
+    <li><i class="fab fa-facebook"></i></li>
+    <li><i class="fab fa-twitter"></i></li>
+    <li><i class="fab fa-instagram"></i></li>
+  </ul>
+  <div class="line2"></div>
+  <h3> {{layer}} <span>/ 05</span></h3>
+  <hr />
+</div>`,
+});
+new Vue({
+  el: "#pagepiling",
+});
 // Menu
 menu = document.querySelector(".nav ul");
 menuToggler = document.querySelector(".nav .menuBtn");
