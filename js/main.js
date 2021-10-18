0584dd64caa4c08a8fa08fe68588a8e78bc1c135
jrazap <elazapm@gmail.com>
2021-10-18 06:23:56 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // Menu
 menu = document.querySelector(".nav ul");
 menuToggler = document.querySelector(".nav .menuBtn");
-
 menuToggler.onclick = () => {
   menuToggler.classList.toggle("fa-times");
   menu.classList.toggle("active");
